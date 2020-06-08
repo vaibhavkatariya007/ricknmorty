9fa04c9fc8a072e22ecc740b8011f7323cdb5b91
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-06-08 09:36:14 +0530

    Updates

---
+++ precache-manifest.327013747594be1bb09761b75e7d0d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8238bae31912470b576ccc8f31503094",
+    "url": "/ricknmorty/index.html"
+  },
+  {
+    "revision": "e3b772624f5e138af6a1",
+    "url": "/ricknmorty/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "11d1a31469f46f389c42",
+    "url": "/ricknmorty/static/css/main.952cf376.chunk.css"
+  },
+  {
+    "revision": "e3b772624f5e138af6a1",
+    "url": "/ricknmorty/static/js/2.0357ebc4.chunk.js"
+  },
+  {
+    "revision": "dc76a30710091997fc3d",
+    "url": "/ricknmorty/static/js/3.5473e312.chunk.js"
+  },
+  {
+    "revision": "11d1a31469f46f389c42",
+    "url": "/ricknmorty/static/js/main.9d50ec3c.chunk.js"
+  },
+  {
+    "revision": "c98e6500c00ddbd3e582",
+    "url": "/ricknmorty/static/js/runtime~main.4a68e785.js"
+  }
+]);
